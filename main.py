4a173587874cbeb4a2d4d9c6d02f4ca1b10c9566
Yusuf Sina <yusufsina.bakan@hotmail.com>
2022-01-25 15:51:55 +0300

    250122 1550 modbus connection

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-import easymodbus.modbusClient
 import window
 from PyQt5.QtWidgets import QApplication
 import sys
-
-modbus_client = easymodbus.modbusClient.ModbusClient('192.168.1.1', 700)
-
-modbus_client.connect()
-if(modbus_client.is_connected):
-    print("Connection established")
-    input_registers = modbus_client.read_holdingregisters(0, 1)
-    print(input_registers)
-    modbus_client.write_single_register(0, 30)
-else:
-    print("Connection Error")
-modbus_client.close()
 
 app = QApplication(sys.argv)
 pencere = window.Pencere()
--- window.py
+++ window.py
@@ -1,8 +1,43 @@
-from PyQt5.QtWidgets import QMainWindow
+from msilib.schema import Error
+from tkinter import E
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
 import Ui_modbus
+import easymodbus.modbusClient
+
 
 class Pencere(QMainWindow, Ui_modbus.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.ip = self.txtIpv1.text() + self.txtIpv2.text() + \
+            self.txtIpv3.text() + self.txtIpv4.text()
+        self.btnConnect.clicked.connect(self.connectToPLC)
+        self.btnRead.clicked.connect(self.readDataFromPLC)
+        self.btnWrite.clicked.connect(self.writeDataToPLC)
 
+    def connectToPLC(self):
+        print(self.ip)
+        modbus_client = easymodbus.modbusClient.ModbusClient(
+            '192.168.1.1', 700)
+        self.modbusClient = modbus_client
+        try:
+            modbus_client.connect()
+        except Error:
+            msg = QMessageBox()
+            QMessageBox.setText("Bir hata oluştu", Error.name)
+            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        if(modbus_client.is_connected):
+            for i in range(0, 50):
+                self.comRead.addItem(str(i))
+                self.comWrite.addItem(str(i))
+
+    def writeDataToPLC(self):
+        if(self.modbusClient.is_connected):
+            self.modbusClient.write_single_register(
+                int(self.comWrite.currentText()), int(self.txtWrite.text()))
+
+    def readDataFromPLC(self):
+        if(self.modbusClient.is_connected):
+            input_registers = self.modbusClient.read_holdingregisters(
+                int(self.comRead.currentText()), 1)
+            self.txtRead.setText(str(input_registers[0]))
